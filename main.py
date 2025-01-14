1ea6235d1bb0b88d392a9b1a0582bda24dba6075
David Berko <daberko364@gmail.com>
2025-01-14 21:45:34 +0000

    split dataset with small normalization error

---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+
+file_path = './data/BC_data.csv'
+
+
+df = pd.read_csv(file_path)
+
+
+X = df.drop('diagnosis', axis=1) # Features
+y = df['diagnosis'] # Target variable
+
+
+# Normalizing the data
+scaler = StandardScaler
+X_scaled = scaler.fit_transform(X)
+
+
+# Splitting the data
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+print('Data Splitting Done!')
